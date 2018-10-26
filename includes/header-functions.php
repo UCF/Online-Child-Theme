585f13411ecc25cd35cdce37aefc6d4298326beb
Jo Dickson <jogreybill@gmail.com>
2018-10-26 13:10:23 -0400

    Initial file setup; added preliminary header style and markup overrides

---
+++ includes/header-functions.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Overrides the UCF WordPress Theme function for returning a site nav to
+ * display nothing.  We do this to more easily implement a custom navbar
+ * in this theme, positioned outside of the .site-header elem.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param bool $image Whether or not a media background is present in the page header (unused)
+ * @return string Nav HTML
+ */
+function ucfwp_get_nav_markup( $image=true ) {
+	return '';
+}
+
+/**
+ * Returns HTML markup for the primary site navigation for UCF Online.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @return void
+ **/
+function online_get_nav_markup() {
+	$title_elem = ( is_home() || is_front_page() ) ? 'h1' : 'span';
+
+	ob_start();
+?>
+	<nav class="site-nav navbar navbar-toggleable-md navbar-custom navbar-light bg-primary sticky-top" role="navigation">
+		<div class="container d-flex flex-row flex-nowrap justify-content-between">
+			<<?php echo $title_elem; ?> class="mb-0">
+				<a class="navbar-brand font-weight-black text-uppercase letter-spacing-1 mr-lg-4" href="<?php echo get_home_url(); ?>"><?php echo bloginfo( 'name' ); ?></a>
+			</<?php echo $title_elem; ?>>
+			<button class="navbar-toggler ml-auto align-self-start collapsed" type="button" data-toggle="collapse" data-target="#header-menu" aria-controls="header-menu" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-text">Menu</span>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse align-self-lg-stretch" id="header-menu">
+				<?php
+				if ( has_nav_menu( 'header-menu' ) ) {
+					wp_nav_menu( array(
+						'container'       => '',
+						'depth'           => 2,
+						'fallback_cb'     => 'bs4Navwalker::fallback',
+						'menu_class'      => 'nav navbar-nav ml-md-auto mr-lg-4',
+						'theme_location'  => 'header-menu',
+						'walker'          => new bs4Navwalker()
+					) );
+				}
+				?>
+				<div class="pb-3 pb-lg-2 pt-lg-2 my-auto mx-3 mx-lg-0">
+					<button class="btn btn-complementary btn-block">TODO CTA Here</button>
+				</div>
+			</div>
+		</div>
+	</nav>
+<?php
+	echo ob_get_clean();
+}
+
+add_action( 'after_body_open', 'online_get_nav_markup', 10, 0 );
+
---
+++ includes/meta.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Enqueue front-end css and js.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ **/
+function online_enqueue_frontend_assets() {
+	// Register child theme stylesheet
+	$theme = wp_get_theme();
+	$theme_version = $theme->get( 'Version' );
+	wp_enqueue_style( 'style-child', ONLINE_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
+}
+
+add_action( 'wp_enqueue_scripts', 'online_enqueue_frontend_assets', 11, 0 );
