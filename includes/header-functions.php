70548817becc9bc7430e4dfa2491766a7adf25c9
Jo Dickson <jogreybill@gmail.com>
2018-11-02 15:01:38 -0400

    Added initial header markup overrides within media headers

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -1,6 +1,19 @@
 <?php
 
 /**
+ * Short-circuit this function from the UCF WordPress Theme to always
+ * return 'title', since page header subtitles are not used in this theme.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param object $obj A WP_Post or WP_Term object
+ * @return string Option value for the designated page header h1
+ **/
+function ucfwp_get_header_h1_option( $obj ) {
+	return 'title';
+}
+
+/**
  * Overrides the UCF WordPress Theme function for returning a site nav to
  * display nothing.  We do this to more easily implement a custom navbar
  * in this theme, positioned outside of the .site-header elem.
@@ -86,4 +99,42 @@ function online_get_navbar_cta_text() {
 		return wptexturize( $val );
 	}
 	return '';
+}
+
+
+/**
+ * Returns inner markup for page headers that use a media background.
+ * Despite the function name, subtitles are not used in this theme.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param object $obj A WP_Post or WP_Term object
+ * @return string HTML for the page title
+ **/
+function ucfwp_get_header_content_title_subtitle( $obj ) {
+	$title      = ucfwp_get_header_title( $obj );
+	$title_elem = ( is_home() || is_front_page() ) ? 'h2' : 'h1';
+
+	ob_start();
+
+	if ( $title ):
+?>
+	<div class="header-content-inner align-self-start pt-4 pt-md-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-9 offset-3 col-md-8 offset-md-4">
+					<div class="row">
+						<div class="col">
+							<<?php echo $title_elem; ?>><?php echo $title; ?></<?php echo $title_elem; ?>>
+						</div>
+						<?php // TODO add header form here in .col if present ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+	endif;
+
+	return ob_get_clean();
 }
