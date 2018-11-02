f9fbc3d85181786e45c03979206c4232a8cd784b
Jo Dickson <jogreybill@gmail.com>
2018-11-02 15:35:08 -0400

    Added ability to specify header text color; moved ACF config location

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -13,6 +13,7 @@ function ucfwp_get_header_h1_option( $obj ) {
 	return 'title';
 }
 
+
 /**
  * Overrides the UCF WordPress Theme function for returning a site nav to
  * display nothing.  We do this to more easily implement a custom navbar
@@ -26,6 +27,88 @@ function ucfwp_get_header_h1_option( $obj ) {
 function ucfwp_get_nav_markup( $image=true ) {
 	return '';
 }
+
+
+/**
+ * Overrides UCF WordPress Theme function to return the markup for
+ * page headers with media backgrounds.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param object $obj A WP_Post or WP_Term object
+ * @param array $videos Assoc. array of video Attachment IDs for use in page header media background
+ * @param array $images Assoc. array of image Attachment IDs for use in page header media background
+ * @return string HTML for the page header
+ **/
+function ucfwp_get_header_media_markup( $obj, $videos, $images ) {
+	$field_id   = ucfwp_get_object_field_id( $obj );
+	$videos     = $videos ?: ucfwp_get_header_videos( $obj );
+	$images     = $images ?: ucfwp_get_header_images( $obj );
+	$video_loop = get_field( 'page_header_video_loop', $field_id );
+	$header_text_color   = get_field( 'page_header_text_color', $field_id );
+	$header_content_type = get_field( 'page_header_content_type', $field_id );
+	$header_height       = get_field( 'page_header_height', $field_id );
+	$exclude_nav         = get_field( 'page_header_exclude_nav', $field_id );
+
+	// We modify the header's text color using bg utilities to make sure we
+	// still meet color contrast req's when bg imgs/videos fail to load
+	$header_bg_class = '';
+	switch ( $header_text_color ) {
+		case 'white':
+			$header_bg_class = 'bg-inverse';
+			break;
+		default:
+			break;
+	}
+
+	ob_start();
+?>
+	<div class="header-media <?php echo $header_height; ?> <?php echo $header_bg_class; ?> mb-0 d-flex flex-column">
+		<div class="header-media-background-wrap">
+			<div class="header-media-background media-background-container">
+				<?php
+				// Display the media background (video + picture)
+
+				if ( $videos ) {
+					echo ucfwp_get_media_background_video( $videos, $video_loop );
+				}
+				if ( $images ) {
+					$bg_image_srcs = ucfwp_get_header_media_picture_srcs( $header_height, $images );
+					echo ucfwp_get_media_background_picture( $bg_image_srcs );
+				}
+				?>
+			</div>
+		</div>
+
+		<?php
+		// Display the inner header contents
+		?>
+		<div class="header-content">
+			<div class="header-content-flexfix">
+				<?php
+				if ( $header_content_type === 'custom' ) {
+					echo ucfwp_get_header_content_custom( $obj );
+				}
+				else {
+					echo online_get_header_content_default( $obj );
+				}
+				?>
+			</div>
+		</div>
+
+		<?php
+		// Print a spacer div for headers with background videos (to make
+		// control buttons accessible), and for headers showing a standard
+		// title/subtitle to push them up a bit
+		if ( $videos || $header_content_type === 'default' ):
+		?>
+		<div class="header-media-controlfix"></div>
+		<?php endif; ?>
+	</div>
+<?php
+	return ob_get_clean();
+}
+
 
 /**
  * Displays the primary site navigation for UCF Online.
@@ -103,15 +186,15 @@ function online_get_navbar_cta_text() {
 
 
 /**
- * Returns inner markup for page headers that use a media background.
- * Despite the function name, subtitles are not used in this theme.
+ * Returns default inner content markup for page headers that
+ * use a media background.
  *
  * @author Jo Dickson
  * @since 1.0.0
  * @param object $obj A WP_Post or WP_Term object
  * @return string HTML for the page title
  **/
-function ucfwp_get_header_content_title_subtitle( $obj ) {
+function online_get_header_content_default( $obj ) {
 	$title      = ucfwp_get_header_title( $obj );
 	$title_elem = ( is_home() || is_front_page() ) ? 'h2' : 'h1';
 
