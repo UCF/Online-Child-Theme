b049d826056a4dd5c8fcb3e0395fc54aac7a60cc
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-11-05 10:47:39 -0500

    Added additional styles to degree-picker

--- includes/degree-search-functions.php
+++ includes/degree-search-functions.php
@@ -13,20 +13,18 @@ if ( ! function_exists( 'ucf_degree_picker_inline_layout' ) ) {
 		<form class="form" action="" method="GET" data-degree-picker>
 			<div class="row">
 				<label class="sr-only" for="sel_program_type">Select Program Type</label>
-				<select class="form-control col-12 col-md mb-2 text-uppercase" id="sel_program_type" name="sel_program_type">
+				<select class="custom-select form-control col-12 col-md mb-2 text-uppercase" id="sel_program_type" name="sel_program_type">
 					<option value="">Degree Level</option>
 				</select>
 				<label class="sr-only" for="sel_interest">Select an Area of Interest</label>
-				<select class="form-control col-12 col-md mb-2 text-uppercase" id="sel_interest" name="sel_interest" disabled>
+				<select class="custom-select form-control col-12 col-md mb-2 text-uppercase" id="sel_interest" name="sel_interest" disabled>
 					<option value="">Interest</option>
 				</select>
 				<label class="sr-only" for="sel_program">Select Program</label>
-				<select class="form-control col-12 col-md mb-2 text-uppercase" id="sel_program" name="sel_program" disabled>
+				<select class="custom-select form-control col-12 col-md mb-2 text-uppercase" id="sel_program" name="sel_program" disabled>
 					<option value="">Program</option>
 				</select>
-			</div>
-			<div class="row justify-content-end">
-				<button type="submit" class="col-sm-4 col-12 btn btn-primary" name="submit" disabled>View Program</button>
+				<button type="submit" class="col-12 col-md-1 btn btn-primary btn-inline h-100" name="submit" disabled><span class="sr-only">View Program</span><span class="fa fa-arrow-right" aria-visible="false"></span></button>
 			</div>
 		</form>
 	<?php
