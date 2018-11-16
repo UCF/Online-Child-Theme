8c7f345801cbcbb9bec5c4768dafb14c59afaedb
Jo Dickson <jogreybill@gmail.com>
2018-11-16 09:04:16 -0500

    Clarified catalog thumbnail instructions

--- includes/config.php
+++ includes/config.php
@@ -211,7 +211,7 @@ function online_define_customizer_fields( $wp_customize ) {
            'degree_catalog_thumbnail',
            array(
 			   'label'       => 'Degree Catalog Thumbnail',
-			   'description' => 'The thumbnail to display on degree information request confirmation pages.',
+			   'description' => 'The catalog thumbnail to display on form redirect confirmation pages. Will only be displayed if the confirmation page is related to a degree, and that degree has a Catalog URL meta value set.',
 			   'section'     => ONLINE_THEME_CUSTOMIZER_PREFIX . 'degrees'
            )
        )
