8f0388e99df5a68307910dcb9db08b79ae22850c
Jo Dickson <jogreybill@gmail.com>
2018-11-19 10:13:40 -0500

    Fixed notices

--- simple-vertical-template.php
+++ simple-vertical-template.php
@@ -5,8 +5,8 @@
  */
 ?>
 <?php get_header(); the_post(); ?>
-<?php echo online_get_degree_typeahead_markup(); ?>
-<?php echo online_get_popular_programs_markup(); ?>
+<?php echo online_get_degree_typeahead_markup( $post ); ?>
+<?php echo online_get_popular_programs_markup( $post ); ?>
 <article class="<?php echo $post->post_status; ?> post-list-item">
 	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
 		<?php the_content(); ?>
