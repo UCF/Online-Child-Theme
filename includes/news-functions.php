55c052c97fec48aeeeef160a6e9f4aa904ee5853
Cadie Brown <cadie.brown@gmail.com>
2018-11-27 15:10:48 -0500

    Add ucf_news_simple_before hook to update the wrapping div's class

--- includes/news-functions.php
+++ includes/news-functions.php
@@ -2,6 +2,18 @@
 /**
  * Adds the simple layout for displaying News stories
  **/
+if ( ! function_exists( 'ucf_news_display_simple_before' ) ) {
+	function ucf_news_display_simple_before( $content, $items, $args, $display_type ) {
+		ob_start();
+	?>
+		<div class="ucf-news ucf-news-simple">
+	<?php
+		return ob_get_clean();
+	}
+	add_filter( 'ucf_news_display_simple_before', 'ucf_news_display_simple_before', 10, 4 );
+}
+
+
 if ( ! function_exists( 'ucf_news_display_simple' ) ) {
 	function ucf_news_display_simple( $content, $items, $args, $display_type, $fallback_message='' ) {
 		if ( $items === false ) {
