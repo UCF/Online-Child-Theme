8841d00d3dc9d7fab3917c3c7ff4c2c4f8f9ee93
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-11-30 15:01:52 -0500

    Code review changes

--- includes/vertical-functions.php
+++ includes/vertical-functions.php
@@ -180,16 +180,20 @@ function online_get_vertical_popular_programs_markup( $post ) {
 		}
 
 		if ( $mode === 'tag_override' ) {
+
 			$override_tags = get_field( 'popular_programs_tag_override', $post->ID );
-			foreach( $override_tags as $ot ) {
-				$tags[] = $ot->slug;
+
+			if ( ! empty( $override_tags ) ) {
+				foreach( $override_tags as $ot ) {
+					$tags[] = $ot->slug;
+				}
 			}
 		} else {
 			$tags[] = 'popular';
 		}
 
 		if ( count( $tags ) > 1 ) {
-			$args['post_tag__and'] = implode( ',', $tags );
+			$args['tag_slug__and'] = implode( ',', $tags );
 		} else {
 			// We still have to parse the array, and this is the safest way
 			$args['tag'] = implode( ',', $tags );
