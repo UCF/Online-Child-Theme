6a636806ae30cb61808771ae27d76441525a0f3f
Cadie Brown <cadie.brown@gmail.com>
2019-02-27 17:00:16 -0500

    Added comments and cleaned up class names and code

--- includes/landing-functions.php
+++ includes/landing-functions.php
@@ -4,7 +4,8 @@
  */
 
 /**
- * Determines which classes to apply to the highlight div based on what landing page template is used.
+ * Determines which classes to apply to the highlight div based on
+ * what landing page template is used.
  *
  * @since 1.2.0
  * @param object $post The landing page WP_Post object
@@ -14,13 +15,16 @@
 function online_highlight_classes( $post ) {
 	$landing_template = get_page_template_slug( $post );
 
+	// Right now, the same set of classes are applied to all landing
+	// page templates except for the default template (which returns
+	// an empty string for get_page_template_slug()).
 	if ( ! empty( $landing_template ) ) {
-		$highlight_col_classes = 'col-12 col-md-4 mb-3 mb-md-0';
+		$col_classes = 'col-12 col-md-4 mb-3 mb-md-0';
 	} else {
-		$highlight_col_classes = 'col-12 mb-3 mb-md-4';
+		$col_classes = 'col-12 mb-3 mb-md-4';
 	}
 
-	return $highlight_col_classes;
+	return $col_classes;
 }
 
 
@@ -42,35 +46,36 @@ function online_get_landing_highlights( $post ) {
 	<div class="row">
 	<?php
 		while ( have_rows( 'landing_highlights', $post->ID ) ) : the_row();
-			$icon_type = get_sub_field( 'landing_highlight_icon_type' );
-			$highlight_content = get_sub_field( 'landing_highlight_content' );
-			$highlight_content_text = $highlight_content['text'];
-			$highlight_content_supplementary_text = $highlight_content['sub_text'];
+			$icon_type        = get_sub_field( 'landing_highlight_icon_type' );
+			$content_text     = get_sub_field( 'landing_highlight_content' )['text'];
+			$content_sup_text = get_sub_field( 'landing_highlight_content' )['sup_text'];
 	?>
 			<div class="<?php echo $col_classes; ?> d-flex">
 				<div class="mr-3">
 				<?php
 				if ( $icon_type === 'image' ) :
-				$image_icon_url = get_sub_field('landing_highlight_icon_image')['url'];
+				$image_icon_url = get_sub_field( 'landing_highlight_icon_image' )['url'];
 				?>
 					<div class="landing-highlight-icon-image-wrapper">
-						<img class="landing-highlight-icon-image" src="<?php echo $image_icon_url; ?>" aria-hidden="true">
+						<img class="landing-highlight-icon-image" src="<?php echo $image_icon_url; ?>" alt="">
 					</div>
-				<?php elseif ( $icon_type === 'fonticon' ) :
-					$font_icon_options = get_sub_field( 'landing_highlight_icon_font_options' );
+				<?php
+				elseif ( $icon_type === 'fonticon' ) :
+					$font_icon_classes = get_sub_field( 'landing_highlight_icon_font_options' )['landing_highlight_icon_font_classes'];
+					$font_icon_color   = get_sub_field( 'landing_highlight_icon_font_options' )['landing_highlight_icon_font_color'];
 				?>
-					<span class="<?php echo $font_icon_options['landing_highlight_icon_font_classes']; ?> fa-3x fa-fw" aria-hidden="true" aria-hidden="true" style="color: <?php echo $font_icon_options['landing_highlight_icon_font_color']; ?>"></span>
+					<span class="<?php echo $font_icon_classes; ?> fa-3x fa-fw" aria-hidden="true" style="color: <?php echo $font_icon_color; ?>"></span>
 				<?php endif; ?>
 				</div>
-				<div class="text">
-					<span class="d-block text-uppercase font-weight-bold"><?php echo $highlight_content_text; ?></span>
-					<span class=""><?php echo $highlight_content_supplementary_text; ?></span>
+				<div class="landing-highlight-content">
+					<span class="landing-highlight-content-text d-block text-uppercase font-weight-bold"><?php echo $content_text; ?></span>
+					<span class="landing-highlight-content-sup-text"><?php echo $content_sup_text; ?></span>
 				</div>
 			</div>
 	<?php endwhile; ?>
 	</div>
-
-	<?php endif;
+	<?php
+	endif;
 
 	return ob_get_clean();
 }
