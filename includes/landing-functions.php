faf2c08eeec49183efa30c56c81d64d9801e3ef7
Cadie Brown <cadie.brown@gmail.com>
2019-02-28 09:58:16 -0500

    Update function name and add acf custom color palette action

--- includes/landing-functions.php
+++ includes/landing-functions.php
@@ -8,11 +8,12 @@
  * what landing page template is used.
  *
  * @since 1.2.0
+ * @author Cadie Brown
  * @param object $post The landing page WP_Post object
  * @return string Column classes for the highlight div
  * @see online_get_landing_highlights()
  **/
-function online_highlight_classes( $post ) {
+function online_get_highlight_classes( $post ) {
 	$landing_template = get_page_template_slug( $post );
 
 	// Right now, the same set of classes are applied to all landing
@@ -33,11 +34,12 @@ function online_highlight_classes( $post ) {
  * or an empty string if none are set for the given landing page.
  *
  * @since 1.2.0
+ * @author Cadie Brown
  * @param object $post The landing page WP_Post object
  * @return string HTML markup
  */
 function online_get_landing_highlights( $post ) {
-	$col_classes = online_highlight_classes( $post );
+	$col_classes = online_get_highlight_classes( $post );
 
 	ob_start();
 
@@ -79,3 +81,30 @@ function online_get_landing_highlights( $post ) {
 
 	return ob_get_clean();
 }
+
+/**
+ * Customizes the color picker default palette for
+ * the highlight color picker ACF field.
+ *
+ * @since 1.2.0
+ **/
+function online_highlight_acf_custom_color_palette() {
+    ?>
+    <script type="text/javascript">
+    (function($){
+
+        acf.add_filter('color_picker_args', function( args, $field ){
+
+			if ($field.context.dataset.name === 'landing_highlight_icon_font_color') {
+				args.palettes = ['#fc0', '#bbb', '#767676', '#000']
+			}
+
+            return args;
+        });
+
+    })(jQuery);
+    </script>
+    <?php
+}
+
+add_action('acf/input/admin_footer', 'online_highlight_acf_custom_color_palette');
