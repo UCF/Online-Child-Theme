b478a9a5318887f9f045f0e6a654a061c7c9dc8c
Cadie Brown <cadie.brown@gmail.com>
2019-02-28 11:53:44 -0500

    Add isset() for image array check

--- template-landing-2.php
+++ template-landing-2.php
@@ -24,7 +24,7 @@ $highlights    = online_get_landing_highlights( $post );
 </div>
 <?php endif; ?>
 
-<?php if ( $images['header_image_xs'] ) : ?>
+<?php if ( isset( $images['header_image_xs'] ) ) : ?>
 <div class="container text-center mt-3 mt-sm-4 hidden-md-up">
 	<img class="img-fluid" src="<?php echo $bg_image_srcs['xs']; ?>" alt="">
 </div>
