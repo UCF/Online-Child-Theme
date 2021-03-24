997efd05dbc6227af6e62c5efd5771389c784d3a
RJ Bruneel <rj.bruneel@ucf.edu>
2021-03-24 16:20:52 -0400

    Updated confirmation template with new layout and fields.

--- template-form-confirmation.php
+++ template-form-confirmation.php
@@ -6,60 +6,67 @@
 <?php get_header(); the_post(); ?>
 
 <?php
-$degree            = null;
-$degree_name       = 'UCF degree';
-$label             = filter_input( INPUT_GET, 'label', FILTER_SANITIZE_STRING );
-$catalog_thumb_url = get_theme_mod( 'degree_catalog_thumbnail' ) ?: online_get_theme_mod_default( 'degree_catalog_thumbnail' ); // Always return a value, even if the customizer option is empty
-$catalog_doc_url   = '';
+$degree             = null;
+$degree_name        = 'UCF degree';
+$label              = filter_input( INPUT_GET, 'label', FILTER_SANITIZE_STRING );
+$catalog_thumb_url  = get_theme_mod( 'degree_catalog_thumbnail' ) ?: online_get_theme_mod_default( 'degree_catalog_thumbnail' ); // Always return a value, even if the customizer option is empty
+$confirmation_video = '';
+$brochure_document  = '';
 
 if ( $label ) {
 	$degree = get_page_by_title( $label, OBJECT, 'degree' );
 }
 
 if ( $degree ) {
-	$degree_name = get_the_title( $degree );
-	$catalog_doc = get_field( 'catalog_document', $degree->ID );
-
-	if ( isset( $catalog_doc ) ) {
-		$catalog_doc_url = $catalog_doc['url'];
-	}
+	$degree_name        = get_the_title( $degree );
+	$confirmation_video = get_field( 'degree_confirmation_video', $degree->ID );
+	$brochure_document  = get_field( 'degree_brochure_document_file', $degree->ID );
 }
 ?>
 
 <article id="form-confirmation">
-	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
 
-		<?php if ( $catalog_doc_url ): ?>
-		<div class="jumbotron bg-faded">
+	<div class="jumbotron bg-faded mb-0">
+		<div class="container">
 			<div class="row">
-				<div class="col-8 offset-2 col-sm-6 offset-sm-3 col-md-4 offset-md-1 mb-5 mb-md-0 text-center">
-					<a target="_blank" href="<?php echo $catalog_doc_url; ?>" class="d-block gtm-viewbook-link">
-						<img class="rotate-left box-shadow-soft img-fluid publication-image" src="<?php echo $catalog_thumb_url; ?>">
-					</a>
-				</div>
-				<div class="col-10 offset-1 col-sm-10 col-md-6 align-self-md-center">
-					<div class="publication-content">
-						<h3 class="h4 text-uppercase">Learn More</h3>
-						<p class="mb-4">Download your information guide for the <strong><?php echo $degree_name; ?></strong> program. It contains additional degree information including course lists, degree requirements, and more.</p>
+				<div class="col-md-6 col-lg-5 align-self-lg-center">
+
+					<?php if ( $confirmation_video ): ?>
+						<div class="embed-responsive embed-responsive-16by9 mb-4 mb-lg-5">
+							<iframe title="UCF Online Thank You" src="<?php echo $confirmation_video; ?>"
+								width="500" height="281" frameborder="0" allow="autoplay; fullscreen; picture-in-picture">
+							</iframe>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( $brochure_document ): ?>
 						<p class="text-center">
-							<a class="btn btn-primary" target="_blank" href="<?php echo $catalog_doc_url; ?>"><span class="fa fa-file-pdf-o" aria-hidden="true"></span> Download Now</a>
+							<a class="btn btn-primary" target="_blank" href="<?php echo $brochure_document; ?>">
+								<span class="fa fa-file-pdf-o" aria-hidden="true"></span> Download Now
+							</a>
 						</p>
-					</div>
+						<p class="mb-4">
+							Download your information guide for the <strong><?php echo $_GET['label']; ?></strong> program. 
+							It contains additional degree information including course lists, degree requirements, and more.
+						</p>
+					<?php endif; ?>
+
+				</div>
+				<div class="col-md-6 col-lg-6 offset-lg-1">
+					<?php the_content(); ?>
 				</div>
 			</div>
 		</div>
-		<?php endif; ?>
-
-		<?php the_content(); ?>
-
-		<?php if ( isset( $_GET['category'] ) && isset( $_GET['action'] ) && isset( $_GET['label'] ) ): ?>
-		<script>
-			if (typeof ga !== 'undefined') {
-				ga('send', 'event', '<?php echo $_GET['category']; ?>', '<?php echo $_GET['action']; ?>', '<?php echo $_GET['label']; ?>');
-			}
-		</script>
-		<?php endif; ?>
 	</div>
+
+	<?php if ( isset( $_GET['category'] ) && isset( $_GET['action'] ) && isset( $_GET['label'] ) ): ?>
+	<script>
+		if (typeof ga !== 'undefined') {
+			ga('send', 'event', '<?php echo $_GET['category']; ?>', '<?php echo $_GET['action']; ?>', '<?php echo $_GET['label']; ?>');
+		}
+	</script>
+	<?php endif; ?>
+
 </article>
 
 <?php get_footer(); ?>
