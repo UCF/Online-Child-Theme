f45022556196cbf1ae172529a86a1725bf32fcb1
RJ Bruneel <rj.bruneel@ucf.edu>
2022-06-22 11:38:03 -0400

    Removed application and admissions sections and added tuition logic.

--- single-degree-acf.php
+++ single-degree-acf.php
@@ -14,11 +14,9 @@
 	get_template_part( 'template-parts/degree/program_at_a_glance' );
 	get_template_part( 'template-parts/degree/description' );
 	get_template_part( 'template-parts/degree/deadlines_apply' );
-	get_template_part( 'template-parts/degree/start_application' );
 	get_template_part( 'template-parts/degree/course_overview' );
 	get_template_part( 'template-parts/degree/quotes' );
 	get_template_part( 'template-parts/degree/skills_careers' );
-	get_template_part( 'template-parts/degree/admission_requirements' );
 	get_template_part( 'template-parts/degree/news' );
 
 	get_template_part( 'template-parts/degree/catalog_modal' );
