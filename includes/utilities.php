f62a53897a50523fb7185e6fb0184f8bd804633e
RJ Bruneel <rj.bruneel@ucf.edu>
2022-06-22 09:43:47 -0400

    Copies fields from Main Site theme and created new ACF Template

--- includes/utilities.php
+++ includes/utilities.php
@@ -3,6 +3,20 @@
  * General utilities
  **/
 
+function format_raw_postmeta( $postmeta ) {
+	$retval = array();
+
+	foreach( $postmeta as $key=>$val ) {
+		if ( is_array( $val ) && count( $val ) === 1 ) {
+			$retval[$key] = $val[0];
+		} else {
+			$retval[$key] = $val;
+		}
+	}
+
+	return online_format_degree_data( $retval );
+}
+
 /**
  * Returns a Online Child Theme mod's default value.
  *
---
+++ single-degree-acf.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: ACF Template
+ * Template Post Type: degree
+ */
+?>
+
+<?php get_header(); the_post(); ?>
+
+<?php
+	$colleges = wp_get_post_terms( $post->ID, 'colleges' );
+	$college  = is_array( $colleges ) ? $colleges[0] : null;
+
+	get_template_part( 'template-parts/degree/program_at_a_glance' );
+	get_template_part( 'template-parts/degree/description' );
+	get_template_part( 'template-parts/degree/deadlines_apply' );
+	get_template_part( 'template-parts/degree/start_application' );
+	get_template_part( 'template-parts/degree/course_overview' );
+	get_template_part( 'template-parts/degree/quotes' );
+	get_template_part( 'template-parts/degree/skills_careers' );
+	get_template_part( 'template-parts/degree/admission_requirements' );
+	get_template_part( 'template-parts/degree/news' );
+
+	get_template_part( 'template-parts/degree/catalog_modal' );
+?>
+
+<?php get_footer(); ?>
