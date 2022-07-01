11ba3f3b1a7074157e0cba1f196a526bdbd57d4e
RJ Bruneel <rj.bruneel@ucf.edu>
2022-07-01 11:50:10 -0400

    code review update

--- template-parts/degree/related-programs.php
+++ template-parts/degree/related-programs.php
@@ -13,7 +13,7 @@ if ( $post->post_type === 'degree' ) :
 			<div class="row">
 				<div class="col-md-12">
 					<h2 class="h3 mb-4">Related Online Programs</h2>
-					<?php echo do_shortcode( '[ucf-post-list post_type="degree" posts_per_page="3" posts_per_row="3" layout="' . $degree_related_programs_layout . '" tag="' . $degree_related_programs_tag . '"]' ); ?>
+					<?php echo do_shortcode( sprintf( '[ucf-post-list post_type="degree" posts_per_page="3" posts_per_row="3" layout="%s" tag="%s"]', $degree_related_programs_layout, $degree_related_programs_tag ) ); ?>
 				</div>
 			</div>
 		</div>
